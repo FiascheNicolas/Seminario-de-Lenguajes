965a2aa4fa4e881151c30d4bf748466758593240
aleortega <alejothomasortega@gmail.com>
2017-09-16 10:16:34 -0300

    +Agrego proyecto inicial.

---
+++ Codigo/Marshmallow Survivor/ArchivoEjemplo.py
@@ -0,0 +1,14 @@
+class ClaseEjemplo(object):
+    '''
+    Clase de prueba para importarla a Main
+    '''
+    def __init__(self):
+        '''Constructor'''
+        
+    def llamadaAClase(self):
+        print "Clase importada correctamente"
+        
+def llamadaAFuncionFueraDeClase():
+    print "Metodo aparte llamado correctamente"
+    
+        
---
+++ Codigo/Marshmallow Survivor/Main.py
@@ -0,0 +1,12 @@
+import ArchivoEjemplo
+#Si creas un paquete, se puede importar asi:
+#from NombrePaquete import NombreDeArchivoPY (sin extension)
+
+#Clase main
+
+objetoClase = ArchivoEjemplo.ClaseEjemplo()
+print objetoClase.llamadaAClase()
+print ArchivoEjemplo.llamadaAFuncionFueraDeClase()  
+    
+    
+    
