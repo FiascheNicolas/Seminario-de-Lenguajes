923e604efb2eb38511df8e35548f623a825a0f88
aleortega <alejothomasortega@gmail.com>
2017-09-26 21:34:36 -0300

    Fix a fallo commit.

--- Codigo/Marshmallow Survivor/Background.py
+++ Codigo/Marshmallow Survivor/Background.py
@@ -1,23 +1,15 @@
 import pygame
-background=pygame.image.load("imagenes/Background/background.1.png")
+
 class Background(pygame.sprite.Sprite):
-    def __init__(self, x, y,alto,ancho):
+    
+    def __init__(self, x, y, alto, ancho):
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
-       
-        
         self.alto = alto
         self.ancho = ancho
-        self.fondo=background
+        self.fondo = pygame.image.load("imagenes/Background/imagen.png").convert_alpha()
         self.image = pygame.transform.scale(self.fondo, (self.alto, self.ancho)) # el alto y ancho debe ser el mismo q del screen
         self.rect = self.image.get_rect()
-        self.rect.x=x #Las posiciones x e y tienen que ser 0,0
-        self.rect.y=y
-    #def update(self, *args):
-        #self.image = pygame.transform.scale(self.fondo, (self.alto, self.ancho))
-        
-        
-            
-        
-       
+        self.rect.x = x                      #Las posiciones x e y tienen que ser 0,0
+        self.rect.y = y
--- Codigo/Marshmallow Survivor/Main.py
+++ Codigo/Marshmallow Survivor/Main.py
@@ -2,55 +2,69 @@ import pygame
 import Malvavisco
 import Chef
 import Background
+import Dulce
+from random import randint
 
 #CONSTANTES
 WIDTH=900
 HEIGHT=500
 FPS=60
-WHITE=(255,255,255)
-BLACK=(0,0,0)
-
-
-spritesPrincipales = pygame.sprite.Group()
-#background=Background.Background(0,0,900,500)
-malvavisco = Malvavisco.Malvavisco(0,-50000,400,400)
-chef = Chef.Chef(100, 0, 300, 300)
-
-
-#spritesPrincipales.add(background)
-spritesPrincipales.add(malvavisco)
-spritesPrincipales.add(chef)
-
 
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Malvavisco")
+screen = pygame.display.set_mode((1366, 768), pygame.FULLSCREEN)
+pygame.display.set_caption("Marshmallow Survivor")
 clock = pygame.time.Clock()
 
+background = Background.Background(0, 0, 1366, 768)
+malvavisco = Malvavisco.Malvavisco(375, 550, 200, 200)
+chef = Chef.Chef(500, 0, 250, 250)
+spritesPrincipales = pygame.sprite.Group()
+spriteBackground = pygame.sprite.Group()
+spritesPrincipales.add(malvavisco)
+spritesPrincipales.add(chef)
+spriteBackground.add(background)
+
+#background = pygame.image.load("imagenes/fondo.jpg").convert_alpha()
 # loop del juego
 
-running =True
+running = True
+listaDulces = []
+spritesDulces = pygame.sprite.Group()
+
 while running:
     #Correr a la velocidad correcta
-    clock.tick(FPS)
     
+    numeroRandom = randint(1, 7)
+    
+    if numeroRandom > 6:
+        nuevoDulce = Dulce.Dulce()
+        spritesDulces.add(nuevoDulce)
+        listaDulces.append(nuevoDulce)
+    
+    clock.tick(FPS)
     #Eventos
     for event in pygame.event.get():
         #chequear si se cierra la ventana
         if event.type==pygame.QUIT:
             running = False
+    
+    for sprite in spritesDulces:
+        
+        if sprite.y > 500:
+            spritesDulces.delete(sprite)
+    
     #Update
-    spritesPrincipales.update()
-    
-    
     #its=pygame.sprite.spritecollide(malvavisco,enemys, False)
     #if hits:
     #   running=False
     #Draws
-    screen.fill(BLACK)
-    
+
+    spriteBackground.draw(screen)
+    spritesDulces.draw(screen)
     spritesPrincipales.draw(screen)
+    spritesPrincipales.update()
+    spritesDulces.update()
     #despues de dibujar,flip the display
     pygame.display.flip()
  
