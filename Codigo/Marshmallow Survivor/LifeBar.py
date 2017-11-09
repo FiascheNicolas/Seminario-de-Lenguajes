b21a284e3d0d0d048f5a909d44093a2fb3da4ecd
MauroLucas <m_l_pereyra@hotmail.com>
2017-11-09 13:16:15 -0300

    Agrego clase LifeBar para manejo de la barra de vida del chef

---
+++ Codigo/Marshmallow Survivor/LifeBar.py
@@ -0,0 +1,48 @@
+import pygame
+from __init__ import cargarDatos
+LIFEBAR = 4
+PATH_LIFEBAR = "imagenes/LifeBar/"
+RED=(255,0,0)
+
+class LifeBar(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, alto, ancho):
+        pygame.sprite.Sprite.__init__(self)
+        #Settings
+        self.x = x
+        self.y = y
+        self.alto = alto
+        self.ancho = ancho
+       
+        self.vidas = 4
+        #
+        #Carga de animaciones
+        self.lifebar = self.cargarAnimacion(LIFEBAR, PATH_LIFEBAR)
+        #
+        
+        #
+        #Settings de sprite
+        self.image = pygame.transform.scale(self.lifebar[self.vidas], (self.alto, self.ancho))
+        self.rect = self.image.get_rect()
+       
+        self.rect.x = x
+        self.rect.y = y
+
+    
+
+    def update(self, *args):
+        self.image = pygame.transform.scale(self.lifebar[self.vidas], (self.alto, self.ancho))
+        
+         
+        
+
+    
+
+    def cargarAnimacion(self, cantidad, path):
+        contador = 0
+        listaAnimacion = []
+        while contador != cantidad + 1:
+            listaAnimacion.append(pygame.image.load(path + str(contador) + ".png").convert_alpha())
+            contador += 1
+
+        return listaAnimacion
